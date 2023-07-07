84cad057cf9a9b3ae35b9de8b9d4d6e268e9ce65
caiovidinha <caiovidinha@id.uff.br>
2023-07-07 18:28:22 -0300

    Adicionando gráfico de barras

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import TopCards from '@/components/TopCards'
+import TopCards from '../components/TopCards'
 import Header from '../components/Header'
+import BarChart from '../components/BarChart'
+import RecentOrders from '../components/RecentOrders'
 
 
 export default function Home() {
@@ -7,6 +9,10 @@ export default function Home() {
     <main className={`bg-gray-100 min-h-screen`}>
     <Header />
     <TopCards />
+    <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
+      <BarChart />
+      <RecentOrders />
+    </div>
     </main>
   )
 }
