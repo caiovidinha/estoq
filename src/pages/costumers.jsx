20fe042823982c6f04c4d7490757ecb31df14409
Caio <caiovidinha@id.uff.br>
2023-07-10 12:22:18 -0300

    Finalizando costumers e orders

--- src/pages/costumers.jsx
+++ src/pages/costumers.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { BsPersonFill, BsThreeDotsVertical } from 'react-icons/bs'
+import { data } from '../data/data'
 
 const costumers = () => {
   return (
-    <div>costumers</div>
+    <div className='bg-gray-100 min-h-screen'>
+
+    	<div className= 'flex justify-between p-4'>
+    	<h2>Costumers</h2>
+    	<h2>Olá, Clint</h2>
+    	</div>
+
+    	<div className='p-4'>
+    		<div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
+				<div className='my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer font-bold'>
+					<span>Nome</span>
+					<span className='sm:text-left text-right'>E-mail</span>
+					<span className='hidden md:grid'>Última doação</span>
+					<span className='hidden md:grid'>Método</span>
+				</div>
+				<ul>
+					{data.map((order,id) => (
+						<li key={id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
+							<div className='flex items-center'>
+								<div className='bg-purple-100 p-3 rounded-lg'>
+									<BsPersonFill className='text-purple-800'/>
+								</div>
+								<p className='pl-4'>{order.name.first + ' ' + order.name.last}</p>
+								
+							</div>
+								<p className='text-gray-600 sm:text-left text-right'>{order.name.first.toLowerCase()}@gmail.com</p>
+								<p className='hidden md:flex '>{order.date}</p>
+							<div className='sm:flex hidden justify-between items-center'>
+								<p>{order.method}</p>
+								<BsThreeDotsVertical />
+							</div>
+						</li>
+					))}
+				</ul>
+        	</div>
+      	</div>
+    </div>
   )
 }
 
