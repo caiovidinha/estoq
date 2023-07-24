6dedc78bb227bf4e80458f410152b33c3554c4e6
caiovidinha <caiovidinha@id.uff.br>
2023-07-24 15:02:37 -0300

    Corrigindo icones

--- src/components/RecentOrders.jsx
+++ src/components/RecentOrders.jsx
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react'
 import { BsThreeDots } from 'react-icons/bs'
-import { BiRestaurant,BiCar,BiPhone,BiSolidTShirt,BiCreditCardAlt,BiBomb,BiSmile,BiHealth,BiGift,BiMoneyWithdraw } from 'react-icons/bi'
-import { GiWeightLiftingUp } from 'react-icons/gi'
+import { BiRestaurant,BiCar,BiPhone,BiSolidTShirt,BiCreditCardAlt,BiBomb,BiSmile,BiGift,BiMoneyWithdraw } from 'react-icons/bi'
+import { GiWeightLiftingUp,GiHealthNormal } from 'react-icons/gi'
 import { SiBetfair,SiFreelancer } from 'react-icons/si'
 import { AiOutlineTool } from 'react-icons/ai'
 import { RiFundsBoxLine,RiBillLine } from 'react-icons/ri'
@@ -36,25 +36,25 @@ const RecentOrders = () => {
 
           <li key={id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer'>
             <div className={mov.tipo==='Receita' ? 'bg-green-200 rounded-lg p-3' :'bg-red-200 rounded-lg p-3'} >
-            { mov.categoria === 'Alimentação' ? <BiRestaurant className='text-red-800'/>
-            : mov.categoria === 'Locomoção' ? <BiCar className='text-red-800'/>
-            : mov.categoria === 'Academia' ? <GiWeightLiftingUp className='text-red-800'/>
-            : mov.categoria === 'Celular' ? <BiPhone className='text-red-800'/>
-            : mov.categoria === 'Vestuário' ? <BiSolidTShirt className='text-red-800'/>
-            : mov.categoria === 'Cartão' ? <BiCreditCardAlt className='text-red-800'/>
-            : mov.categoria === 'Dívida' ? <BiBomb className='text-red-800'/>
-            : mov.categoria === 'Lazer' ? <BiSmile className='text-red-800'/>
-            : mov.categoria === 'Saúde' ? <BiHealth className='text-red-800'/>
-            : mov.categoria === 'Bet' ? <SiBetfair className='text-red-800'/>
-            : mov.categoria === 'Presente' ? <BiGift className='text-red-800'/>
-            : mov.categoria === 'Serviços' ? <AiOutlineTool className='text-red-800'/>
-            : mov.categoria === 'Investimento' ? <RiFundsBoxLine  className={mov.tipo === 'Receita' ? 'text-green-800' : 'text-red-800'}/>
-            : mov.categoria === 'Fatura' ? <BiRestaurant className='text-red-800'/>
-            : mov.categoria === 'Salário' ? <BiMoneyWithdraw  className='text-green-800'/>
-            : mov.categoria === 'Freelance' ? <SiFreelancer  className='text-green-800'/>
-            : mov.categoria === 'Limite Cartão' ? <BiCreditCardAlt  className='text-green-800'/>
-            :  <BsThreeDots  className={mov.tipo === 'Receita' ? 'text-green-800' : 'text-red-800'}/>
-            
+            { mov.categoria === 'Alimentação' ? <BiRestaurant size={20} className='text-red-800'/>
+            : mov.categoria === 'Locomoção' ? <BiCar size={20} className='text-red-800'/>
+            : mov.categoria === 'Academia' ? <GiWeightLiftingUp size={20} className='text-red-800'/>
+            : mov.categoria === 'Celular' ? <BiPhone size={20} className='text-red-800'/>
+            : mov.categoria === 'Vestuário' ? <BiSolidTShirt size={20} className='text-red-800'/>
+            : mov.categoria === 'Cartão' ? <BiCreditCardAlt size={20} className='text-red-800'/>
+            : mov.categoria === 'Dívida' ? <BiBomb size={20} className='text-red-800'/>
+            : mov.categoria === 'Lazer' ? <BiSmile size={20} className='text-red-800'/>
+            : mov.categoria === 'Saúde' ? <GiHealthNormal size={20} className='text-red-800'/>
+            : mov.categoria === 'Bet' ? <SiBetfair size={20} className='text-red-800'/>
+            : mov.categoria === 'Presente' ? <BiGift size={20} className='text-red-800'/>
+            : mov.categoria === 'Serviços' ? <AiOutlineTool size={20} className='text-red-800'/>
+            : mov.categoria === 'Investimento' ? <RiFundsBoxLine size={20}  className={mov.tipo === 'Receita' ? 'text-green-800' : 'text-red-800'}/>
+            : mov.categoria === 'Fatura' ? <RiBillLine size={20} className='text-red-800'/>
+            : mov.categoria === 'Salário' ? <BiMoneyWithdraw size={20}  className='text-green-800'/>
+            : mov.categoria === 'Freelance' ? <SiFreelancer size={20}  className='text-green-800'/>
+            : mov.categoria === 'Limite Cartão' ? <BiCreditCardAlt size={20}  className='text-green-800'/>
+            :  <BsThreeDots size={20}  className={mov.tipo === 'Receita' ? 'text-green-800' : 'text-red-800'}/>
+
           }
             </div>
             <div className='pl-4'>
