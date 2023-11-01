4228ca0abfc36a70f4591b77a445aef38d9d7d00
Caio Vidinha <caiovidinha@id.uff.br>
2023-11-01 15:03:37 -0300

    Balanço mensal

--- src/pages/mes.jsx
+++ src/pages/mes.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Progress } from '@nextui-org/react'
-import { BsCreditCardFill } from 'react-icons/bs'
+import { BsFillCalendarCheckFill } from 'react-icons/bs'
 import { Balanco } from '@/components/Balanco'
 
 const mes = () => {
@@ -32,6 +32,8 @@ const mes = () => {
 
     const [balanco, setBalanco] = useState([])
 
+    let hoje = new Date().toISOString()
+    hoje = hoje.slice(5, 7)
     let SHEET_ID = '1kusPEM4OdchOyHp7Coa7MfB0Nnq3SUqWCxH0PGW5ldE'
     let SHEET_TITLE = 'Dashboard'
     let SHEET_RANGE = 'A:C'
@@ -49,10 +51,15 @@ const mes = () => {
             let data = JSON.parse(rep.substr(47).slice(0, -2))
             let balanco = new Balanco()
             for (let i = 0; i < data.table.rows.length; i++) {
+                let valor = data.table.rows[i].c[2].v.toFixed(2)
+                valor = valor + ''
+                valor = parseFloat(valor.replace(/[\D]+/g, ''))
+                valor = valor + ''
+                valor = valor.replace(/([0-9]{2})$/g, ',$1')
                 balanco.salvar(
                     data.table.rows[i].c[0].v,
                     data.table.rows[i].c[1].v,
-                    data.table.rows[i].c[2].v.toFixed(2)
+                    valor
                 )
             }
             setBalanco(balanco.arrayBal)
@@ -66,36 +73,65 @@ const mes = () => {
                         <span>Balanço Mensal</span>
                     </div>
                     <ul>
-                        {balanco
-                            .slice(0)
-                            .reverse()
-                            .map((bal, index) => (
-                                <li
-                                    key={index}
-                                    className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer"
-                                >
-                                    <div className="flex items-center justify-between">
-                                        <div className="rounded-full p-3 bg-blue-200">
-                                            <BsCreditCardFill className="text-blue-900" />
-                                        </div>
+                        {balanco.map((bal, index) => (
+                            <li
+                                key={index}
+                                className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid grid-cols-2 items-center justify-between cursor-pointer"
+                            >
+                                <div className="w-full flex items-center justify-center sm:justify-left">
+                                    <div
+                                        className={
+                                            bal.mes == hoje
+                                                ? 'rounded-full p-3 bg-green-200 -ml-10 sm:-ml-0 mr-4 sm:mr-0'
+                                                : 'rounded-full p-3 bg-blue-200 -ml-10 sm:-ml-0 mr-4 sm:mr-0'
+                                        }
+                                    >
+                                        <BsFillCalendarCheckFill
+                                            className={
+                                                bal.mes == hoje
+                                                    ? 'text-green-900'
+                                                    : 'text-blue-900'
+                                            }
+                                        />
+                                    </div>
+                                    <p className="ml-0 sm:ml-10">
                                         {bal.mes + '/' + bal.ano}
+                                    </p>
+                                </div>
+
+                                <div className="-ml-16 sm:-ml-96 flex w-64 sm:w-full">
+                                    <div className="ml-8 sm:ml-10 mr-3 sm:mr-10 mt-1 sm:mt-1.5 w-24 sm:w-9/12">
+                                        <Progress
+                                            className="w-24 sm:w-96"
+                                            color={
+                                                parseFloat(bal.valor) > 1000
+                                                    ? 'success'
+                                                    : parseFloat(bal.valor) > 0
+                                                    ? 'warning'
+                                                    : 'danger'
+                                            }
+                                            value={(
+                                                ((3800 -
+                                                    parseFloat(bal.valor)) *
+                                                    100) /
+                                                3800
+                                            ).toFixed(1)}
+                                        />
                                     </div>
-                                    <div>
-                                        <div className="w-28">
-                                            <Progress
-                                                color="secondary"
-                                                value={(
-                                                    ((3800 - bal.valor) * 100) /
-                                                    3800
-                                                ).toFixed(1)}
-                                            />
-                                        </div>
-                                        <p className="text-right text-xs text-gray-400 w-32">
-                                            R$ {bal.valor} / R$ 3800.00
-                                        </p>
+                                    <div
+                                        className={
+                                            parseFloat(bal.valor) > 1000
+                                                ? 'text-green-700 font-bold'
+                                                : parseFloat(bal.valor) > 0
+                                                ? 'text-yellow-600 font-bold'
+                                                : 'text-red-700 font-bold'
+                                        }
+                                    >
+                                        {'R$ ' + bal.valor}
                                     </div>
-                                </li>
-                            ))}
+                                </div>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
