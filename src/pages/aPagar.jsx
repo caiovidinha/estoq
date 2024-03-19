5e08b25b1be75fd1002b7380d4e93f6b66be3476
Caio Vidinha <caiovidinha@id.uff.br>
2024-03-19 13:01:01 -0300

    add apagar e araeceebr

--- src/pages/aPagar.jsx
+++ src/pages/aPagar.jsx
@@ -308,7 +308,8 @@ const apagar = () => {
                         pag += parseFloat(movimentacao[i].valor.toString().replace(',','.'))
                     }
                 }
-                // console.log(rec,pag)
+                setPagar(pag)
+                setReceber(rec)
             }
     })
     return (
@@ -381,7 +382,7 @@ const apagar = () => {
 
                         </select></div>
                     </div>
-                        <div className="w-full items-end justify-end flex"></div>
+                        <div className="w-full items-end md:justify-end justify-around flex my-2 p-3 bg-gray-200 rounded-lg"><p className='font-bold'>A receber:&nbsp;</p>R${receber ? receber.toString().replace('.',',') : '0,00'}&nbsp;|&nbsp;<p className='font-bold'>A pagar:&nbsp;</p>R${pagar ? (-pagar).toString().replace('.',','): '0,00'}</div>
                     <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-3 items-center justify-between font-bold">
                         <span>Movimentação</span>
                         <span className="sm:text-left text-right">Status</span>
