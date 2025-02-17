b351e868b35bcf0b32039c099be27958526ea4a9
caiovidinha <caiovidinha@id.uff.br>
2025-02-17 12:16:29 -0300

    import react in contextr=

--- src/components/CategoryDropdown.jsx
+++ src/components/CategoryDropdown.jsx
@@ -1,4 +1,5 @@
 import { useState, useMemo, useEffect } from 'react';
+import React from 'react';
 import { Dropdown } from '@nextui-org/react';
 import { fetchCategories } from '@/utils/getCategories'; // Função centralizada para buscar categorias
 
